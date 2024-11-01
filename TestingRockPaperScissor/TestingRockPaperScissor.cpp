1ba6f612a777d8e762bc52c65a4036823acc61a5
Birendra Deuba <Bdeuba7977@conestogac.on.ca>
2024-11-01 18:41:51 -0400

    Revert "#commit1 test case for either player 1 or player 2 win or draws"
    This reverts commit e2432411f59b10ea0abac8a6d8bcde31252cdcef.

--- TestingRockPaperScissor/TestingRockPaperScissor.cpp
+++ TestingRockPaperScissor/TestingRockPaperScissor.cpp
@@ -10,52 +10,8 @@ namespace TestingRockPaperScissor
 	{
 	public:
 		
-        // Test cases for Player 1 wins
-            TEST_METHOD(Player1WinsRockVsScissors)
-        {
-            Assert::AreEqual("Player1", winner_is("Rock", "Scissors"));
-        }
-
-        TEST_METHOD(Player1WinsPaperVsRock)
-        {
-            Assert::AreEqual("Player1", winner_is("Paper", "Rock"));
-        }
-
-        TEST_METHOD(Player1WinsScissorsVsPaper)
-        {
-            Assert::AreEqual("Player1", winner_is("Scissors", "Paper"));
-        }
-
-        // Test cases for Player 2 wins
-        TEST_METHOD(Player2WinsRockVsPaper)
-        {
-            Assert::AreEqual("Player2", winner_is("Rock", "Paper"));
-        }
-
-        TEST_METHOD(Player2WinsPaperVsScissors)
-        {
-            Assert::AreEqual("Player2", winner_is("Paper", "Scissors"));
-        }
-
-        TEST_METHOD(Player2WinsScissorsVsRock)
-        {
-            Assert::AreEqual("Player2", winner_is("Scissors", "Rock"));
-        }
-
-        // Test cases for draws
-        TEST_METHOD(DrawRockVsRock)
-        {
-            Assert::AreEqual("Draw", winner_is("Rock", "Rock"));
-        }
-
-        TEST_METHOD(DrawPaperVsPaper)
-        {
-            Assert::AreEqual("Draw", winner_is("Paper", "Paper"));
-        }
-
-        TEST_METHOD(DrawScissorsVsScissors)
-        {
-            Assert::AreEqual("Draw", winner_is("Scissors", "Scissors"));
-        }
+		TEST_METHOD(TestMethod1)
+		{
+		}
 	};
 }
